604b2f02e6de72cc4454d9dc9455cedd69f2c9d8
Administrator <2498106333@qq.com>
2020-11-25 09:37:12 +0800

    小毕提交项目

---
+++ src/test/java/com/bgs/biddingbs/BiddingbsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.bgs.biddingbs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runner.Runner;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.swing.*;
+
+@SpringBootTest
+@RunWith(Runner.class)
+class BiddingbsApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+
+}
